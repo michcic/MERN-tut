72e3a133535db9d7bdc95bdfddf351e3ee81c023
varenghar <varenghar@users.noreply.github.com>
2019-07-03 19:43:44 +0100

    iint

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoos = require('mongoose')
+
+const UserSchema = new mongoos.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = User = mongoos.model('user', UserSchema)
